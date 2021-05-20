3cc728a00fd08876bfb40b70643476ab8f8311e3
heartrainy <526132448@qq.com>
2021-05-20 15:28:49 +0800

    fix: xiugai

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectListScreen() {
+  return (
+    <SearchPanel>
+      ProjectList
+    </SearchPanel>
+  );
+}
+
+export default ProjectListScreen;
---
+++ src/screens/project-list/seach-panel.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+function SeachPanel() {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch('').then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param])
+
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) => setParam({ ...param, personId: evt.target.value })}
+        >
+          <option value={""}>负责人</option>
+        </select>
+      </div>
+    </form>
+  );
+}
+
+export default SeachPanel;
